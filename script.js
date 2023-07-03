61035eb79251d9c65ffcf0a70ae8ae7a56f3663a
reasonsharma123 <reasonsharma34@gmail.com>
2023-07-03 22:12:13 +0545

    check num

---
+++ script.js
@@ -0,0 +1,14 @@
+function ispositive(num){
+    if(num>0){
+        return"is positive number";
+    }
+    else if (num==0){
+        return "is negative number";
+    }
+    else if(num==0){
+        return"Given number is zero";
+    }
+}
+console.log(ispositive(3))
+console.log(ispositive(4))
+console.log(ispositive(0))
